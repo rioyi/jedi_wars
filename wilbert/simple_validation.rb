780fc8a100f29bfade7c41700ab2c6c714e44421
Wilbert <wilbert.angarita@gmail.com>
2016-09-29 15:33:16 -0400

    Teminado Simple validation

--- wilbert/simple_validation.rb
+++ wilbert/simple_validation.rb
@@ -8,3 +8,22 @@ length shoould be between 4 and 16 characters.
 def validate_usr(username)
 (username =~ /\A[a-z0-9_]{4,16}\z/) == 0
 end
+
+=begin
+
+Otras respuestas:
+
+def validate_usr(username)
+  /\A[a-z0-9_]{4,16}\z/ === username
+end
+------------------------------------------
+def validate_usr(username)
+  username =~ /^[a-z0-9_]{4,16}$/ ? true : false
+end
+
+------------------------------------------------
+def validate_usr(username)
+ !! username.match(/(?=\w{4,16})^[a-z0-9_]+$/)
+end
+
+=end
